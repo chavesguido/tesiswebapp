72af67e3c7462f104e188e16acca85454580e0a1
chavesguido <chavesguido@hotmail.com>
2018-10-01 00:52:04 -0300

    commiteando perri

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
+//Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { ParticlesModule } from 'angular-particle';
 
 
+//Components
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home//home.component';
 
+//Services
+import { RestLoginService } from './services/login/restLogin.service';
+import { RestNuevaCuentaService } from './services/crearCuenta/restNuevaCuenta.service';
+
+//Rutas de la aplicación
+import { APP_ROUTING } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING,
+    ParticlesModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    RestLoginService,
+    RestNuevaCuentaService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
